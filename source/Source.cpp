04d7d5c359c1a61949dd76cd25cea2f96a2b55e7
Vidar Ekblad <66982832+Vadru93@users.noreply.github.com>
2020-12-10 16:19:52 +0100

    Minor update to alt+enter keystate

--- source/Source.cpp
+++ source/Source.cpp
@@ -5047,7 +5047,7 @@ bool bToggleWindowed = false;
 
 SHORT __stdcall proxy_GetAsyncKeyState(int key)
 {
-    if (KeyState::GetKeyboardState(VirtualKeyCode::ENTER) && GetAsyncKeyState(VK_MENU) < 0)//alt + enter
+    if (KeyState::GetKeyboardState(VirtualKeyCode::ENTER) && KeyState::GetKeyboardState(VirtualKeyCode::ALT))//alt + enter
     {
         //Unpress the enter KeyboardState
         KeyState::Unpress(VirtualKeyCode::ENTER);
