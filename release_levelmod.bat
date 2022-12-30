3e79ce5226162a43d3b74bbd93946e71727b9399
DCxDemo <ultrataber@gmail.com>
2022-12-30 13:40:58 +0200

    added bat scripts
    - added release script, fixed old one to copy correct paths
    - added uninstaller script, allows to remove levelmod completely, restoring vanilla game

---
+++ release_levelmod.bat
@@ -0,0 +1,31 @@
+rem - --- RELEASE.BAT: LevelMod release
+rem - --- use Notepad++ for nice syntax highlighting ---
+
+rem - !!! variables !!! change, if needed !!!
+
+	set sevenz="C:\Program Files\7-Zip\7z.exe"
+	set target=release_temp
+
+rem - prepare paths
+
+	cd /d "%~dp0"
+	mkdir %target%
+
+rem - copy files
+
+	copy "LevelMod\d3d8d.dll" "%target%\d3d8.dll"
+	copy "README.MD" "%target%\LevelMod_ReadMe.txt"
+	copy "uninstall_levelmod.bat" "%target%\uninstall_levelmod.bat"
+	%systemroot%\System32\xcopy "%~dp0\LevelMod\Data\*.qb" "%target%\Data" /E /I /H /Y
+	%systemroot%\System32\xcopy "%~dp0\LevelMod\Data\*.txt" "%target%\Data" /E /I /H /Y
+
+rem - pack to zip
+
+	del levelmod_release.zip
+	%sevenz% a -tzip "levelmod_release.zip" ".\%target%\*"
+
+rem - remove temp dirs
+
+	rmdir /s /q %target%
+
+	echo "done!"
---
+++ uninstall_levelmod.bat
@@ -0,0 +1,20 @@
+echo "THPS3 LevelMod Uninstall"
+
+@ECHO OFF
+
+:choice
+set /P c=Are you sure you want to uninstall LevelMod [Y/N]?
+if /I "%c%" EQU "Y" goto :unins
+goto :choice
+
+:unins
+
+rmdir /s /q "%~dp0\data\scripts\levelmod"
+del "%~dp0\data\scripts\levelmod\qdir_lm.txt"
+del "%~dp0\LevelMod.ini"
+del "%~dp0\d3d8.dll"
+del "%~dp0\levelmod_readme.txt"
+del "%~dp0\uninstall_levelmod.bat"
+
+:choice
+pause
